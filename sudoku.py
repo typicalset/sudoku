ac46b8ee2d08b2d5f441b42446990cd1854fb4f8
Matthew Ho <hmat22@gmail.com>
2014-11-03 18:41:02 -0500

    added cooling rate and reheating

--- sudoku.py
+++ sudoku.py
@@ -7,6 +7,12 @@ from matplotlib import pyplot as plt
 BOX_SIZE = 3
 NUM_BOXES = 9
 
+def check_board(board):
+	if board.shape != (NUM_BOXES,NUM_BOXES):
+		raise Exception("Board is of wrong dimensions!")
+	if (board<0).any() or (board>=(NUM_BOXES+1)).any():
+		raise Exception("Numbers must be integers between 1 and 9!")
+
 def box2coord(box):
 	return BOX_SIZE*(box/BOX_SIZE), BOX_SIZE*(box%BOX_SIZE)
 
@@ -15,13 +21,16 @@ def pick_random_box():
 
 class Sudoku:
 
-	T = 0.1
-
 	def __init__(self, fname='sample1.csv'):
+		self.load_board(fname)
+
+	def load_board(self, fname):
 		self.board = np.loadtxt(fname,delimiter=',',dtype=int)
+		check_board(self.board)
 		self.board_initial = self.board == 0
 		self.fname = fname
 		self.print_board()
+		print 'Squares to fill: ' + str(sum(sum(self.board_initial)))
 
 	def find_missing_numbers(self,x,y):
 		missing_numbers = []
@@ -45,7 +54,7 @@ class Sudoku:
 		for i in np.arange(0, NUM_BOXES):
 			self.fill_box(i)
 
-	def compute_energy(self, board):
+	def compute_energy(self):
 		num_duplicates = 0
 		for i in np.arange(0, NUM_BOXES):
 			num_duplicates += 2*NUM_BOXES - len(np.unique(self.board[i,:])) - len(np.unique(self.board[:,i]))
@@ -70,6 +79,7 @@ class Sudoku:
 	def is_done(self):
 		return (np.sum(self.board, 0) == np.sum(self.board,1)).all()
 
+	# print the sudoku board in a user friendly format
 	def print_board(self):
 		print ''
 		for i in np.arange(0,NUM_BOXES):
@@ -82,52 +92,71 @@ class Sudoku:
 			sys.stdout.write('\n')
 		print ''
 
-	def solve(self, nitr=100):
+	def solve(self):
+
+		# these parameters were tuned
+		T = 2.0
+		cooling_interval = 500
+		cooling_rate = 0.9
+		reheat_level = 0.05
+		T_reheat = 0.5
 
 		tstart = time.time()
-		# do stuff
-		
 
 		self.fill_initial()
 
 		E_hist = []
+		deltaE_hist = []
 
-		for i in np.arange(0,nitr):
-			E = self.compute_energy(self.board)
+		num_rejects = 0
+
+		nitr = 0
+
+		while True:
+			E = self.compute_energy()
+			if E == 0:
+				# we're done
+				break
 			E_hist.append(E)
 
 			box = pick_random_box()
 			square1, square2 = self.propose_swap(box)
 			self.swap_squares(square1, square2)
-			E_proposal = self.compute_energy(self.board)
+			E_proposal = self.compute_energy()
 
-			if E_proposal >= E:
-				if np.exp(-(E_proposal-E)/self.T) < np.random.random():
-					self.swap_squares(square1, square2)
-				else:
-					pass # accept proposal
+			deltaE = E_proposal-E
+			deltaE_hist.append(deltaE)
+
+			if np.random.rand() < np.exp(-deltaE/T):
+				# accept
+				pass
 			else:
-				pass # accept proposal
+				# reject, revert back to old configuration
+				self.swap_squares(square1, square2)
+
+			if nitr % cooling_interval == 0:
+				T = T*cooling_rate
+				# "barrier avoidance" - avoid settling into local minima
+				if (T < reheat_level):
+					T = T_reheat
+
+			nitr += 1
+			if nitr % 20000 == 0:
+				t_elapsed = time.time() - tstart
+				print str(round(t_elapsed,0)) +' seconds...'
 
 		t_elapsed = time.time() - tstart
+		print ' ' + str(round(t_elapsed,3)) +' seconds.'
 
 		print 'Solved in ' + str(round(t_elapsed,3)) +' seconds.'
 		self.print_board()
 
-		#plt.plot(E_hist)
+		plt.plot(E_hist)
+		plt.xlabel('iteration')
+		plt.ylabel('Energy')
 
 		# save
 		f, ext = os.path.splitext(self.fname)
 		fname_sol = f+'_sol'+ext
-		np.savetxt(fname_sol, self.board, delimiter=',')
+		np.savetxt(fname_sol, self.board, delimiter=',', fmt='%d')
 		print 'Solution saved in ' + fname_sol
-
-
-
-
-
-
-
-
-
-
